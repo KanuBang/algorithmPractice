06bc08d5bd3ed01e08122946dc8d280383bd3fbf
KanuBang <102508014+KanuBang@users.noreply.github.com>
2023-06-15 00:03:24 +0900

    Update Math3009.py

--- Math/Math3009.py
+++ Math/Math3009.py
@@ -2,24 +2,19 @@ import sys
 input = sys.stdin.readline
 
 x = []
-xSet = []
 y = []
-ySet = []
-
+ans = []
 for i in range(0,3):
     msg = input().rstrip().split()
     x.append(int(msg[0]))
     y.append(int(msg[1]))
 
-xSet = list(x)
-ySet = list(y)
+ans.append(x)
+ans.append(y)
 
+for k in range(0,2):
+    for i in range(0,len(ans[k])):
+        if ans[k].count(ans[k][i]) == 1:
+            print(ans[k][i], end = " ")
+            break
 
-for i in range(0,len(xSet)):
-    if x.count(xSet[i]) == 1:
-        print(xSet[i], end = " ")
-        break
-for i in range(0,len(ySet)):
-    if y.count(ySet[i]) == 1:
-        print(ySet[i])
-        break
