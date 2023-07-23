c3ae1bac9dae5dde5f1457fa21f04395019b45c2
gdrffg <gdrffg@gmail.com>
2023-07-23 16:37:11 +0900

    Create Sort9076.py

---
+++ Sort/Sort9076.py
@@ -0,0 +1,19 @@
+'''
+1 초	128 MB
+
+Ni(1 ≤ Ni ≤ 10, i = 1, 2, ..., 5) 이 정도 범위라서 시공간 제약은 거의 없다고 본다
+'''
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+ans = []
+for i in range(n):
+    myList = list(map(int, input().split()))
+    sortedArr = sorted(myList,key = lambda x: x, reverse=False)
+    if max(sortedArr[1:4]) - min(sortedArr[1:4]) >= 4:
+        ans.append("KIN")
+    else:
+        ans.append(sum(sortedArr[1:4]))
+
+[print(i) for i in ans]
