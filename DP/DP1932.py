de6913dcc03cd04e579d41c9dd55ac69119f79b4
gdrffg <gdrffg@gmail.com>
2023-09-23 16:10:47 +0900

    Create DP1932.py

---
+++ DP/DP1932.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+nums = []
+start,end = (0,-1)
+for i in range(n):
+    nums.append(list(map(int,input().split())))
+
+
+for i in range(1,n):
+    size = len(nums[i])
+    end += 1
+    for j in range(size):
+        if j == start:
+            nums[i][j] += nums[i-1][start]
+        elif j == size - 1:
+            nums[i][j] += nums[i-1][end]
+        else:
+            nums[i][j] += max(nums[i-1][j],nums[i-1][j-1])
+    
+
+
+print(max(nums[i]))
+        
+
