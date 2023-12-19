cb5929332c90f21c9a1f21367a0f9b9c6cb6bf12
gdrffg <gdrffg@gmail.com>
2023-12-19 15:42:59 +0900

    Update graph11725.py

--- Graph/silver/graph11725.py
+++ Graph/silver/graph11725.py
@@ -9,6 +9,7 @@
 '''
 1. 정보를 입력받는다
 2. 입력 받은 정보로 그래프를 만든다.
+3. 
 '''
 
 '''
@@ -16,4 +17,54 @@
  4  6
 2 7  3
       5
+
 '''
+
+'''
+def dfs(graph, node, visited):
+    
+    visitied[node] = True
+    for i in graph[node]:
+        if not visited[i]:
+            parent[i] = node
+            dfs(graph, i, visited)
+
+'''
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+n = int(input())
+graph = [ [] for i in range(n+1)]
+visitied = [False] * (n+1)
+# DFS를 이용했다면 BFS로
+
+def bfs(graph,node, visited):
+    queue = deque([node])
+    visited[node] = True
+    parent = [None] * (n+1)
+
+    while queue:
+        cur = queue.popleft()
+        for i in graph[cur]:
+            if not visited[i]:
+                parent[i] = cur
+                queue.append(i)
+                visited[i] = True
+    return parent
+
+
+for i in range(n-1):
+    x,y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+parent = bfs(graph, 1, visitied)
+[print(i) for i in parent[2:]]
+
+
+
+
+
+
+
