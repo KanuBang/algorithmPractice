5e6277fc8c5530e3283a86187ecda48dde643a15
gdrffg <gdrffg@gmail.com>
2023-12-21 15:26:49 +0900

    Update graph2667.py

--- Graph/silver/graph2667.py
+++ Graph/silver/graph2667.py
@@ -21,7 +21,7 @@ def dfs(x,y):
     
     if graph[x][y] == 1:
         cnt += 1
-        graph[x][y] += 1
+        graph[x][y] = 0
         dfs(x-1,y)
         dfs(x,y-1)
         dfs(x+1,y)
@@ -34,10 +34,10 @@ result = 0
 ans = []
 for i in range(n):
     for j in range(n):
-        cnt = 0
         if dfs(i,j) == True:
             result += 1
             ans.append(cnt)
+            cnt = 0
 
 print(result)
 [print(i) for i in sorted(ans)]
