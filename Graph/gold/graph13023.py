de2c5d22dd408122fe4b2989c47e5f23cf0b46cf
gdrffg <gdrffg@gmail.com>
2023-12-28 15:07:02 +0900

    Update graph13023.py

--- Graph/gold/graph13023.py
+++ Graph/gold/graph13023.py
@@ -1,14 +1,7 @@
 import sys
 sys.setrecursionlimit(10**4)
 input = sys.stdin.readline
-'''
-5 5
-0 1
-1 3
-1 4
-4 3
-3 2
-'''
+
 n,m = map(int, input().split())
 nodes = [n for n in range(n)]
 graph = [[] for _ in range(n)]
@@ -24,11 +17,9 @@ for i in range(m):
 def dfs(graph, i, visited, route):
     visited[i] = True
     route.append(i)
-    #print(f"r: {route}")
     if len(route) == 5:
         print(1)
         exit()
-
 
     for j in graph[i]:
         if not visited[j]:
@@ -36,14 +27,20 @@ def dfs(graph, i, visited, route):
     
     visited[route.pop()] = False
 
-    return route
-
-
 for i in nodes:
     visited = [None] * (n)
     dfs(graph,i,visited, [])
 
 print(0)
+
+'''
+5 5
+0 1
+1 3
+1 4
+4 3
+3 2
+'''
     
     
 
