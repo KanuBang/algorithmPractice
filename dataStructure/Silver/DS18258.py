b9248b1aaf10724214ada1f565fff696b8626bc8
gdrffg <gdrffg@gmail.com>
2024-01-06 16:36:14 +0900

    Solve: DS18258.py

---
+++ dataStructure/Silver/DS18258.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n = int(input())
+
+# push, pop, front, back, empty, size:
+
+
+queue = deque()
+def isEmpty(queue):
+    return  False if queue else True
+
+while n > 0:
+    command = input().split()
+    if command[0] == 'push':
+        queue.append(int(command[1]))
+    
+    elif command[0] == 'size':
+        print(len(queue))
+    
+    elif command[0] == 'empty':
+        print(1) if isEmpty(queue) else print(0)
+
+    elif command[0] == 'pop':
+        print(-1) if isEmpty(queue) else print(queue.popleft())
+
+    elif command[0] == 'front':
+        print(-1) if isEmpty(queue) else print(queue[0])
+
+    elif command[0] == 'back':
+        print(-1) if isEmpty(queue) else print(queue[-1])
+
+    n -= 1
