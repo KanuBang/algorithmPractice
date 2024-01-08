9c4550cf9bd49814b2871d71c502c1276df301b8
gdrffg <gdrffg@gmail.com>
2024-01-08 17:28:39 +0900

    Solve: DS10866.py

---
+++ dataStructure/Silver/DS10866.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n = int(input())
+
+queue = deque()
+def isEmpty(queue):
+    return not queue
+
+while n > 0:
+    command = input().split()
+
+    if command[0] == 'push_back':
+        queue.append(int(command[1]))
+    
+    elif command[0] == 'push_front':
+        queue.appendleft(int(command[1]))
+
+    elif command[0] == 'pop_back':
+        print(-1) if isEmpty(queue) else print(queue.pop())
+    
+    elif command[0] == 'pop_front':
+        print(-1) if isEmpty(queue) else print(queue.popleft())
+
+    elif command[0] == 'size':
+        print(len(queue))
+    
+    elif command[0] == 'empty':
+        print(1) if isEmpty(queue) else print(0)
+
+    elif command[0] == 'front':
+        print(-1) if isEmpty(queue) else print(queue[0])
+
+    elif command[0] == 'back':
+        print(-1) if isEmpty(queue) else print(queue[-1])
+
+    n -= 1
