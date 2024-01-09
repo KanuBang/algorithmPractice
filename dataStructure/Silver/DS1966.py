0f6434887be12fb09cebb4542c75b4a97023445c
gdrffg <gdrffg@gmail.com>
2024-01-09 22:51:24 +0900

    Solve: DS1966.py

--- dataStructure/Silver/DS1966.py
+++ dataStructure/Silver/DS1966.py
@@ -3,39 +3,43 @@ from collections import deque
 input = sys.stdin.readline
 
 n = int(input())
-'''
-1 0
-5
 
-idx: 0 -> 5
-sort -> 5
-5는 첫 번째로 인쇄
-
-4 2
-1 2 3 4
-idx:2 -> 3
-2 3 4 1
-3 4 1 2
-4 1 2 3
-
-
-6 0
-1 1 9 1 1 1
-idx:0 -> 1a
-
-1a 1b 9 1c 1d 1e
-idx: 0 -> 1a
-
-1b 9 1c 1d 1e 1a
-9 1c 1d 1e 1a 1b
-
-
-'''
 while n > 0:
-    numberOfDocs, target = map(int,input().split())
-    queue = deque(list(dict(map(int, input().split()))))
-    
-    print(queue)
 
+    numberOfDocs, targetIdx = map(int,input().split())
+    importances = list(map(int, input().split()))
+    queue = deque([[i, importances[i]] for i in range(len(importances))])
+    printedOrder = 0
 
+    while True:
+        beforeSize = len(queue)
+        front = queue.popleft()
+
+        for item in queue:
+            if item[1] > front[1]:
+                queue.append(front)
+                break
+        
+        nowSize = len(queue)
+        
+        if beforeSize != nowSize:
+            printedOrder += 1
+        
+            if front[0] == targetIdx:
+                print(printedOrder)
+                break
+        
     n -= 1
+
+
+'''
+idx:2 -> 3은 몇번째로 출력되는가?
+1: [0,1] [1,2] [2,3] [3,4]
+2: [1,2] [2,3] [3,4] [0,1]
+3: [2,3] [3,4] [0,1] [1,2]
+4: [3,4] [0,1] [1,2] [2,3] -> 4 print(popleft) (cnt = 1)
+5: [0,1] [1,2] [2,3]
+6: [1,2] [2,3] [0,1]
+7: [2,3] [0,1] [1,2] -> 3 print(popleft) (cnt = 2) -> idx:2 break
+
+'''
